87fe2b28e8a08598673e30af5bbfdb4effb9ec8d
ShahHassanAfridi <shahhassan.hp78@gmail.com>
2025-01-19 12:43:51 +0500

    name add
    my name add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="h-screen w-100% justify-center content-center">
     <h1 className="text-3xl font-bold underline text-blue-400 text-center">
-      Hello world!
+      Hello world! Its Shah Hassan Afridi
     </h1>
     </div>
   )
