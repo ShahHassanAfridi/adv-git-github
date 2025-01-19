cf20246a70f77fb3d99d9e1269f8b16533476dd8
ShahHassanAfridi <shahhassan.hp78@gmail.com>
2025-01-19 12:49:41 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
     <h1 className="text-3xl font-bold underline text-blue-400 text-center">
       Hello world! Its Shah Hassan Afridi
     </h1>
+    <p className="text-3xl font-bold underline text-red-400 text-center">I am a Web Developer as well as a UI/UX designer</p>
     </div>
   )
 }
